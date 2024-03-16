875f9eb7b4233c17d83972f8905086d22cf7babd
lego <oleg.lukasonok@lt.ibm.com>
2024-03-16 23:46:12 +0200

    ...

---
+++ .scripts/yarn/lib/--index-api.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------------
+#
+# PLACEHOLDER_COPYRIGHT
+#
+# PLACEHOLDER_LICENCE
+#
+# -------------------------------------------------------------------------------------
+
+. ./.scripts/yarn/lib/-package-info.sh
+. ./.scripts/yarn/lib/-package-install.sh
+. ./.scripts/yarn/lib/-package-uninstall.sh
